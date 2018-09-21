172255f71a60b8c379c194d0041bbdb7becec82d
Joshua Foss <jmfoss98@gmail.com>
2018-09-21 13:44:06 -0400

    fixing syntax errors

--- Card.cpp
+++ Card.cpp
@@ -10,19 +10,19 @@ std::ostream& operator<<(std::ostream& os, Rank rank)
 {
   switch (rank)
   {
-    case 0: return os << '2';
-    case 1: return os << '3';
-    case 2: return os << '4';
-    case 3: return os << '5';
-    case 4: return os << '6';
-    case 5: return os << '7';
-    case 6: return os << '8';
-    case 7: return os << '9';
-    case 8: return os << 'T';
-    case 9: return os << 'J';
-    case 10: return os << 'Q';
-    case 11: return os << 'K';
-    case 12: return os << 'A';
+    case 0: return os << "2";
+    case 1: return os << "3";
+    case 2: return os << "4";
+    case 3: return os << "5";
+    case 4: return os << "6";
+    case 5: return os << "7";
+    case 6: return os << "8";
+    case 7: return os << "9";
+    case 8: return os << "T";
+    case 9: return os << "J";
+    case 10: return os << "Q";
+    case 11: return os << "K";
+    case 12: return os << "A";
 
   }
 
@@ -32,9 +32,9 @@ std::ostream& operator<<(std::ostream& os, Suit suit)
 {
   switch (suit)
   {
-    case 0: return os << 'S';
-    case 1: return os << 'C';
-    case 2: return os << 'D';
-    case 3: return os << 'H';
+    case 0: return os << "S";
+    case 1: return os << "C";
+    case 2: return os << "D";
+    case 3: return os << "H";
   }
 }
