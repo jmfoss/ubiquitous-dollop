24a8c171ed8f5ad7b701ad1d727ea29c13f7c8a3
Joshua Foss <jmfoss98@gmail.com>
2018-09-21 14:24:41 -0400

    fixing operator overload

--- Game.cpp
+++ Game.cpp
@@ -83,8 +83,8 @@ void Game::start()
   {
     Card one_temp = pop(playerOne);
     Card two_temp = pop(playerTwo);
-    std::cout << one_temp.get_rank() << one_temp.get_suit() << " "
-              << two_temp.get_rank() << one_temp.get_suit() << std::endl;
+    std::cout << one_temp << " "
+              << two_temp << std::endl;
     if (one_temp.get_rank() >= two_temp.get_rank())
     {
       playerOne.push_back(one_temp);
@@ -131,37 +131,4 @@ void Game::display_hands()
     std::cout << i << ": " << card.get_rank() << ", " << card.get_suit() << std::endl;
   }
   return;
-}
-
-std::ostream& operator<<(std::ostream& os, Rank rank)
-{
-  switch (rank)
-  {
-    case 0: return os << "2";
-    case 1: return os << "3";
-    case 2: return os << "4";
-    case 3: return os << "5";
-    case 4: return os << "6";
-    case 5: return os << "7";
-    case 6: return os << "8";
-    case 7: return os << "9";
-    case 8: return os << "T";
-    case 9: return os << "J";
-    case 10: return os << "Q";
-    case 11: return os << "K";
-    case 12: return os << "A";
-
-  }
-
-}
-
-std::ostream& operator<<(std::ostream& os, Suit& suit)
-{
-  switch (suit)
-  {
-    case 0: return os << "S";
-    case 1: return os << "C";
-    case 2: return os << "D";
-    case 3: return os << "H";
-  }
 }
