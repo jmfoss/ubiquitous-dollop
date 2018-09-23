1ca1ff5b6cecd98aa947bc9d8dee111f84105d1f
Joshua Foss <jmfoss98@gmail.com>
2018-09-23 14:24:12 -0400

    added min and max

--- main.cpp
+++ main.cpp
@@ -4,18 +4,28 @@
 int main()
 {
   Game war;
-  int num = 100;
-  int total_turns = 0;
-  for (int i = 0, turns = 0; i < num; ++i)
+  const int NUM_GAMES = 100;
+  int total_turns = 0, min_turns = 0, max_turns = 0;
+  for (int i = 0, turns = 0; i < NUM_GAMES; ++i)
   {
     turns = war.start();
+    if (turns > max_turns)
+    {
+      max_turns = turns;
+    }
+    else if (turns < min_turns || min_turns == 0)
+    {
+      min_turns = turns;
+    }
     total_turns += turns;
     std::cout << "Number of turns for game: " << turns << std::endl;
     war.shuffle();
     war.split();
   }
-  std::cout << std::endl << "Averge amount of turns per game: "
-            << total_turns / num;
+  std::cout << "Shortest game by turns: " << min_turns << std::endl;
+  std::cout << "Longest game by turns: " << max_turns << std::endl;
+  std::cout << "Averge amount of turns per game: "
+            << total_turns / NUM_GAMES;
 
   return 0;
 }
