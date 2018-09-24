ae31b9227206bade89298f45aff908aeaed09366
Joshua Foss <jmfoss98@gmail.com>
2018-09-24 10:40:32 -0400

    changed how spoils are handled

--- Game.hpp
+++ Game.hpp
@@ -11,12 +11,15 @@ class Game
 private:
   std::deque<Card> deck;
   std::deque<Card> playerOne;
+  std::deque<Card> oneSpoils;
   std::deque<Card> playerTwo;
+  std::deque<Card> twoSpoils;
+  std::deque<Card> spoils;
   std::deque<Card> make_deck();
   Card pop(std::deque<Card>& playerDeck);
 public:
   Game();
-  std::pair<Card, Card> war();
+  void war();
   int start();
   void shuffle();
   void split();
