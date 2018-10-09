1a8b0b536097ed406b2937cda21acf185876431c
Joshua Foss <jmfoss98@gmail.com>
2018-10-09 17:20:43 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "war.hpp"
+#include "stats.hpp"
+
+#include <ctime>
+#include <iostream>
+#include <random>
+
+int
+main()
+{
+  std::random_device rd;
+
+  unsigned seed = rd();
+  std::default_random_engine rng(seed);
+  Accumulator acc(seed);
+
+  // unsigned seed = 0;
+  // std::default_random_engine rng(seed);
+  // Accumulator acc(seed);
+
+  for (int i = 0; i < 10000; ++i) {
+    War w(rng, &acc);
+    w.play();
+  }
+
+  std::cout << acc.get_stats();
+
+}
